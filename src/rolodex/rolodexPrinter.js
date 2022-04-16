5343f93fcaa773c13a72702ed36acb6ed8eab732
ductran67 <Josephductran67@gmail.com>
2022-04-16 15:44:50 -0700

    Update rolodexPrinter.js

--- src/rolodex/rolodexPrinter.js
+++ src/rolodex/rolodexPrinter.js
@@ -6,7 +6,7 @@ people.forEach((person) => {
   // const lastName = names[1];
   // const { email } = person;
   // const { phone } = person;
-  const [name, email, phone] = [person.name, person.email, person.phone];
+  const { name, email, phone } = person;
   const [firstName, lastName] = name.split(' ');
 
   console.log(`First name: ${firstName}\nLast name: ${lastName}\nEmail: ${email}\nPhone number: ${phone}\n`);
