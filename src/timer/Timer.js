9d89c7d7e72ff82f82e6a81ff1844d1e96ea7035
ductran67 <Josephductran67@gmail.com>
2022-04-16 13:58:14 -0700

    Week1-assigment completed.

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,17 +1,17 @@
-function Timer(seconds) {
-  this.seconds = seconds;
+class Timer {
+  constructor(seconds) {
+    this.seconds = seconds;
+  }
+
+  start() {
+    const timerInterval = setInterval(() => {
+      if (this.seconds === 0) {
+        clearInterval(timerInterval);
+      }
+      console.log(this.seconds);
+      this.seconds -= 1;
+    }, 1000);
+  }
 }
-
-Timer.prototype.start = function () {
-  var instance = this;
-  var timerInterval = setInterval(function () {
-    if (instance.seconds === 0) {
-      clearInterval(timerInterval);
-    }
-
-    console.log(instance.seconds);
-    instance.seconds -= 1;
-  }, 1000);
-};
 
 export default Timer;
