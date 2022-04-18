5d7d957adbc90bdaf8fe461f4dbe44464f80febc
Joel Elizaga <joel@elizaga.org>
2022-04-18 16:01:41 -0700

    Timer.js complete.

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,16 +1,17 @@
-function Timer(seconds) {
-  this.seconds = seconds;
+class Timer {
+  constructor(seconds) {
+    this.seconds = seconds;
+  }
 }
 
-Timer.prototype.start = function () {
-  var instance = this;
-  var timerInterval = setInterval(function () {
-    if (instance.seconds === 0) {
+Timer.prototype.start = () => {
+  const timerInterval = setInterval(() => {
+    if (this.seconds === 0) {
       clearInterval(timerInterval);
     }
 
-    console.log(instance.seconds);
-    instance.seconds -= 1;
+    console.log(this.seconds);
+    this.seconds -= 1;
   }, 1000);
 };
 
