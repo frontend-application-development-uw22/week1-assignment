94819fbbf6e43fd39ad4e4f4b6675b755b224c49
Joel Elizaga <joel@elizaga.org>
2022-04-18 15:27:14 -0700

    HTMLElement.js complete.

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,19 @@
-// Define class here
+// HTMLElement ////////////////////////////////////////////////////////////////
+// An HTML [tag] and its inner [content].
+class HTMLElement {
+  // constructor ////////////////////////////////////////////////////////////
+  // Constructs HTMLElement with given [tag] and [content].
+  constructor(tag, content) {
+    this.tag = tag;
+    this.content = content;
+  }
 
-// Export class here
-export default {};
+  // render /////////////////////////////////////////////////////////////////
+  // Returns a string with this HTMLElement's [content] wrapped in this
+  // HTMLElement's [tag].
+  render() {
+    return `<${this.tag}>${this.content}</${this.tag}>`;
+  }
+}
+
+export default { HTMLElement };
