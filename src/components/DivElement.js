65e90adbb3daa4d047cc0e5c24cdb95d99e3fbd4
RainyCityDiver <cvmembership@posteo.net>
2022-04-20 10:05:15 -0700

    DivElement and HTMLElement in-progress.

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,19 @@
 // Import HTMLElement here
 
+import { HTMLElement } from "./HTMLElement.js";
+
 // Define class here
 
+class DivElement extends HTMLElement {
+    constructor ( content ) {
+        super();
+        this.content = content;
+    }
+    render(tag, content) {
+        super.render();
+    }
+}
+
 // Export class here
+
+export {DivElement};
