4ce7b0a713b4d12f673a451256cce4f7ceef9e38
pablomarcel <pablomarcel@outlook.com>
2022-04-20 11:16:48 -0700

    run lint, updated code

--- src/rolodex/rolodexPrinter.js
+++ src/rolodex/rolodexPrinter.js
@@ -6,6 +6,6 @@ people.forEach((person) => {
   const lastName = names[1];
   const { email } = person;
   const { phone } = person;
-
+  // eslint-disable-next-line no-console
   console.log(`First name: ${firstName}\nLast name: ${lastName}\nEmail: ${email}\nPhone number: ${phone}\n`);
 });
--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -2,13 +2,13 @@ function Timer(seconds) {
   this.seconds = seconds;
 }
 
-Timer.prototype.start = function () {
+Timer.prototype.start = () => {
   const instance = this;
-  var timerInterval = setInterval(() => {
+  const timerInterval = setInterval(() => {
     if (instance.seconds === 0) {
       clearInterval(timerInterval);
     }
-
+    // eslint-disable-next-line no-console
     console.log(instance.seconds);
     instance.seconds -= 1;
   }, 1000);
