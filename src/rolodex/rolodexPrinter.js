889e3c5778a9a280c2adba67c1b3aa9b928a709e
pablomarcel <pablomarcel@outlook.com>
2022-04-20 17:37:44 -0700

    finished rolodexPrinter.js

--- src/rolodex/rolodexPrinter.js
+++ src/rolodex/rolodexPrinter.js
@@ -1,9 +1,8 @@
 import people from './people.json';
 
 people.forEach((person) => {
-  const names = person.name.split(' ');
-  const firstName = names[0];
-  const lastName = names[1];
+  const { name } = person;
+  const { firstName, lastName } = name.split(' ');
   const { email } = person;
   const { phone } = person;
   // eslint-disable-next-line no-console
