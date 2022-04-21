db9989d2c356bc99650f200be22f24659f607d13
Narayan Veeramani <narayan2709@gmail.com>
2022-04-21 12:18:31 -0700

    assignment 1

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,10 @@
 // Import HTMLElement here
-
+import HTMLElement from './HTMLElement';
 // Define class here
-
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super('div', content);
+  }
+}
 // Export class here
+export default DivElement;
--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,16 @@
 // Define class here
+class HTMLElement {
+  constructor(tag, content) {
+    this.tag = tag;
+    this.content = content;
+  }
+
+  render() {
+    const htmlelement = `<${this.tag}>${this.content}</${this.tag}>`;
+    console.log(htmlelement);
+    return htmlelement;
+  }
+}
 
 // Export class here
-export default {};
+export default HTMLElement;
