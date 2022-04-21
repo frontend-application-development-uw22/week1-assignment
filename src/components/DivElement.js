fbf88a72f84eff54bbded0296645e9d44e25859c
Dan Cartwright <f.d.cartwright@hotmail.com>
2022-04-21 10:11:20 -0700

    saving work

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,17 @@
 // Import HTMLElement here
 
+import HTMLElement from './HTMLElement.js';
+
 // Define class here
 
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super(content);
+    this.tag = 'div';
+    this.content = content;
+  }
+}
+
 // Export class here
+
+export default DivElement;
--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,17 +1,35 @@
-function Timer(seconds) {
-  this.seconds = seconds;
+class Timer {
+  constructor(seconds) {
+    this.seconds = seconds;
+  }
+
+  start() {
+    const timerInterval = setInterval(() => {
+      if (this.seconds === 0) {
+        clearInterval(timerInterval);
+      }
+      this.seconds -= 1;
+      console.log(this.seconds);
+    }, 1000);
+  }
 }
 
-Timer.prototype.start = function () {
-  var instance = this;
-  var timerInterval = setInterval(function () {
-    if (instance.seconds === 0) {
-      clearInterval(timerInterval);
-    }
-
-    console.log(instance.seconds);
-    instance.seconds -= 1;
-  }, 1000);
-};
-
 export default Timer;
+
+// function Timer(seconds) {
+//   this.seconds = seconds;
+// }
+
+// Timer.prototype.start = function () {
+//   const instance = this;
+//   const timerInterval = setInterval(function () {
+//     if (instance.seconds === 0) {
+//       clearInterval(timerInterval);
+//     }
+
+//     console.log(instance.seconds);
+//     instance.seconds -= 1;
+//   }, 1000);
+// };
+
+// export default Timer;
