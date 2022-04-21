eb238e524218c6ac07a8a21b7ed0b77f510c2a05
JCahill <cahilljm@aol.com>
2022-04-20 18:40:27 -0700

    Remove render() from DivElement to resolve test error

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,16 +1,16 @@
-
 // Import HTMLElement here
-import HTMLElement from './HTMLElement'
+import HTMLElement from './HTMLElement';
 
 // Define class here
 class DivElement extends HTMLElement {
   constructor(content) {
     super('div', content);
+    this.content = content;
   }
 
-  render() {
-    super.render();
-  }
-};
+  // render() {
+  //   super.render();
+  // }
+}
 // Export class here
 export default DivElement;
