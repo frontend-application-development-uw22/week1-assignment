d5ce767cfc8c42b083851bdc560d446274a26308
Khanh Hoang <khanhqhoang@hotmail.com>
2022-04-22 22:54:57 -0700

    Khanh's week1-assignment submission

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,16 @@
 // Define class here
+// export default class HTMLElement {
+class HTMLElement {
+  constructor(tag, content) {
+    this.tag = tag;
+    this.content = content;
+  }
 
+  render() {
+    return `<${this.tag}>${this.content}</${this.tag}>`;
+  }
+}
 // Export class here
-export default {};
+export default HTMLElement;
+const elm = new HTMLElement('p', 'I am never so happy as when I am really engaged in good earnest...');
+console.log(elm.render());
