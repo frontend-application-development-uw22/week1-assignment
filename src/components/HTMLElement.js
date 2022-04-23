76be9412b47f624a7f426b0744cb26034bd86cf1
miguerurso <miggle.marin@gmail.com>
2022-04-23 15:54:27 -0700

    Added all HW Responses

--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -1,4 +1,18 @@
 // Define class here
+class HTMLElement {
+  constructor(tag, content) {
+    this.tag = tag;
+    this.content = content;
+  }
 
+  render() {
+    return `<${this.tag}>${this.content}</${this.tag}>`;
+  }
+}
+
+// const p = new HTMLElement('p','heyyyy');
+// console.log(p.render());
+
+// console.log(p);
 // Export class here
-export default {};
+export default HTMLElement;
--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,17 +1,30 @@
-function Timer(seconds) {
-  this.seconds = seconds;
+class Timer {
+  constructor(seconds) {
+    this.seconds = seconds;
+  }
+
+  start() {
+    const timerInterval = setInterval(() => {
+      if (this.seconds === 0) {
+        clearInterval(timerInterval);
+      }
+
+      console.log(this.seconds);
+      this.seconds -= 1;
+    }, 1000);
+  }
 }
 
-Timer.prototype.start = function () {
-  var instance = this;
-  var timerInterval = setInterval(function () {
-    if (instance.seconds === 0) {
-      clearInterval(timerInterval);
-    }
+// Timer.prototype.start = () => {
+//   let instance = this;
+//   let timerInterval = setInterval( () => {
+//     if (instance.seconds === 0) {
+//       clearInterval(timerInterval);
+//     }
 
-    console.log(instance.seconds);
-    instance.seconds -= 1;
-  }, 1000);
-};
+//     console.log(instance.seconds);
+//     instance.seconds -= 1;
+//   }, 1000);
+// };
 
 export default Timer;
