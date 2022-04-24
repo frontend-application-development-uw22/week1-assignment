f16d38f8f64b2bf6f94473770a5f78523cb317ed
Arthur Rodrigues <artrodrig3@outlook.com>
2022-04-24 08:41:14 -0700

    Complete activity 3 of homework.

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,17 +1,19 @@
-function Timer(seconds) {
-  this.seconds = seconds;
-}
+class Timer {
+    constructor(seconds) {
+        this.seconds = seconds;
+    };
 
-Timer.prototype.start = function () {
-  var instance = this;
-  var timerInterval = setInterval(function () {
-    if (instance.seconds === 0) {
-      clearInterval(timerInterval);
+    start() {
+        let instance = this.seconds;
+        let timerInterval = setInterval(() => {
+            if(instance === 0) {
+                clearInterval(timerInterval)
+            }
+
+            console.log(instance);
+            instance--;
+        }, 1000);
     }
-
-    console.log(instance.seconds);
-    instance.seconds -= 1;
-  }, 1000);
-};
+}
 
 export default Timer;
