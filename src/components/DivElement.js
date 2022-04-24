d8cb3fa0e21ef4b43581369620a40092acf631dc
Arthur Rodrigues <artrodrig3@outlook.com>
2022-04-23 23:16:23 -0700

    Complete activity 2 of homework.

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,21 @@
 // Import HTMLElement here
+import HTMLElement from './HTMLElement'
 
 // Define class here
+class DivElement extends HTMLElement {
+    constructor(content) {
+        super('div', content);
+    }
+    
+    render(){
+        // Instructions unclear.
+        // Sounds like this is what you want:
+        return `<${tag}>${content}</${tag}>`;
+
+        // Easier if it was
+        // return super.render();
+    }
+}
 
 // Export class here
+export default DivElement;
