f7ea39d6540f272d655ea4dc8a9d3da964e69d23
carlitos-206 <carloscaceres041@gmail,com>
2022-04-23 19:04:38 -0700

    hw complete

---
+++ src/pythonIdentationTest/pythonTest.py
@@ -0,0 +1,15 @@
+# this is a test for the indentation of python code using 2 space indentation
+
+def hello():
+  return "hello world"
+
+print(hello())
+
+def addition(a,b):
+  sum = a +b 
+  return sum
+
+print(addition(2,2))
+
+
+# Summary: no errors are caused by new indentation rule
--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,17 +1,39 @@
-function Timer(seconds) {
-  this.seconds = seconds;
+// function Timer(seconds) {
+//   this.seconds = seconds;
+// }
+
+// Timer.prototype.start = function () {
+//   const instance = Timer();
+//   const timerInterval = () => {
+//     setInterval(() => {
+//       if (instance.seconds === 0) {
+//         clearInterval(timerInterval);
+//       }
+
+//       console.log(instance.seconds);
+//       instance.seconds -= 1;
+//     }, 1000);
+//   };
+//   return timerInterval;
+// };
+
+class Timer {
+  constructor(seconds) {
+    this.seconds = seconds;
+  }
+
+  start() {
+    /* this is the equivalent of let seconds = this.seconds
+    but this is as an object */
+    let { seconds } = this.seconds;
+    const timerInterval = setInterval(() => {
+      if (seconds === 0) {
+        clearInterval(timerInterval);
+      } else {
+        seconds -= 1;
+      }
+    }, 1000);
+  }
 }
-
-Timer.prototype.start = function () {
-  var instance = this;
-  var timerInterval = setInterval(function () {
-    if (instance.seconds === 0) {
-      clearInterval(timerInterval);
-    }
-
-    console.log(instance.seconds);
-    instance.seconds -= 1;
-  }, 1000);
-};
 
 export default Timer;
