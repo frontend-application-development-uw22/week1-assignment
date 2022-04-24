13a70de28a7b180499cb225816776e55ba65b46c
RainyCityDiver <cvmembership@posteo.net>
2022-04-23 19:30:11 -0700

    In process

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,19 +1,18 @@
 class Timer {
   constructor(seconds) {
-    // this.seconds = seconds;
+    this.seconds = seconds;
   }
 
-  start(seconds) {
-    setInterval(() => {
-      // if (seconds === 0) {
-      //   clearInterval(timerInterval);
-      // }
+  start() {
+    const timerInterval = setInterval(() => {
+      if (this.seconds === 0) {
+        clearInterval(timerInterval);
+      }
 
-      console.log(seconds);
-      seconds -= 1;
+      console.log(this.seconds);
+      this.seconds -= 1;
     }, 1000);
   }
 }
 
 export default Timer;
- 
