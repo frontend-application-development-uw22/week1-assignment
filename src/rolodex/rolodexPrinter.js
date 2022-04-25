b345ce5c215f8b2424078ff7a80bf36acc6b282a
Arthur Rodrigues <artrodrig3@outlook.com>
2022-04-24 20:40:05 -0700

    Complete activity 4 of homework.

--- src/rolodex/rolodexPrinter.js
+++ src/rolodex/rolodexPrinter.js
@@ -1,11 +1,8 @@
 import people from './people.json';
 
-people.forEach(function (person) {
-  const names = person.name.split(' ');
-  const firstName = names[0];
-  const lastName = names[1];
-  const email = person.email;
-  const phone = person.phone;
+people.forEach(person => {
+    const [firstName, lastName] = person.name.split(' ');
+    const {email, phone} = person;
 
-  console.log('First name: ' + firstName + '\nLast name: ' + lastName + '\nEmail: ' + email + '\nPhone number: ' + phone + '\n');
+  console.log(`First name: ${firstName}\nLast name: ${lastName}\nEmail: ${email}\nPhone number: ${phone}\n`);
 });
