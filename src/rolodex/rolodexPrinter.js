69ad307717b12e7426f2051993ac177465314b99
jl105 <jilinda10@gmail.com>
2022-04-25 13:28:55 -0700

    Yuhong Week1 Assignment

--- src/rolodex/rolodexPrinter.js
+++ src/rolodex/rolodexPrinter.js
@@ -2,8 +2,7 @@ import people from './people.json';
 
 people.forEach((person) => {
   const names = person.name.split(' ');
-  const firstName = names[0];
-  const lastName = names[1];
+  const [firstName, lastName] = names;
   const { email, phone } = person;
 
   console.log(`First name: ${firstName} \nLast name: ${lastName} \nEmail: ${email}\nPhone number: ${phone}\n`);
