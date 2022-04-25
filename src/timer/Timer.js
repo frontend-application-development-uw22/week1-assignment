0497c206cf74c9e3ed3dfda9bbfe8da9fe6aa098
jl105 <jilinda10@gmail.com>
2022-04-25 13:42:31 -0700

    Update Timer.js

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,16 +1,18 @@
-function Timer(seconds) {
-  this.seconds = seconds;
+class Timer {
+  constructor(seconds) {
+    this.seconds = seconds;
+  }
+
+  start() {
+    const timerInterval = setInterval(() => {
+      if (this.seconds === 0) {
+        clearInterval(timerInterval);
+      }
+
+      console.log(this.seconds);
+      this.seconds -= 1;
+    }, 1000);
+  }
 }
-
-Timer.prototype.start = function () {
-  const timerInterval = setInterval(function () {
-    if (this.seconds === 0) {
-      clearInterval(timerInterval);
-    }
-
-    console.log(this.seconds);
-    this.seconds -= 1;
-  }, 1000);
-};
 
 export default Timer;
