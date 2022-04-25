7a7ce95f9080a51109cf9048fad22aa7e2aa967a
jl105 <jilinda10@gmail.com>
2022-04-25 12:36:56 -0700

    Yuhong Week 1 Assignment
    Yuhong Week 1 Assignment

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,20 @@
 // Import HTMLElement here
+import HTMLElement from "./HTMLElement";
 
 // Define class here
+class DivElement extends HTMLElement {
+    constructor(content) {
+        super('div', content);
+        this.content = content;
+      }
+  
+      render() {
+          return `<div>${this.content}</div>`;
+      }
+}
 
+const andIThinkToMyself = new DivElement('What a wonderful world');
+
+console.log(andIThinkToMyself.render());
 // Export class here
+export default DivElement;
