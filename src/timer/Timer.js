7f7b6967ea4e45bbacac1115bb4784ba8a4f67ba
Ryan Ku <rjhk@uw.edu>
2022-04-26 02:39:20 -0700

    fixed typo in Timer.js

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,17 +1,18 @@
 class Timer {
-  constuctor(seconds) {
+  constructor(seconds) {
     this.seconds = seconds;
   }
 
   start() {
-    const instance = this;
+    console.log(this.seconds);
+
     const timerInterval = setInterval(() => {
-      if (instance.seconds === 0) {
+      if (this.seconds === 0) {
         clearInterval(timerInterval);
       }
 
-      console.log(instance.seconds);
-      instance.seconds -= 1;
+      console.log(this.seconds);
+      this.seconds -= 1;
     }, 1000);
   }
 }
