cf5b2d1452ab6eacd8ea171b5ac6657fc93883e3
Ryan Ku <rjhk@uw.edu>
2022-04-26 01:12:13 -0700

    fixed rolodexPrinter.js

--- src/rolodex/rolodexPrinter.js
+++ src/rolodex/rolodexPrinter.js
@@ -1,14 +1,14 @@
 import people from './people.json';
 
 people.forEach((person) => {
-  const { firstName, lastName } = person.name.split(' ');
+  const { name } = person;
   const { email, phone } = person;
+  const [firstName , lastName] = name.split(' ');
 
   console.log(
-    `First name: ${firstName}
-  Last name: ${lastName}
-  Email: ${email} 
-  Phone number: ${phone}
-  `,
-  );
+`First name: ${firstName}
+Last name: ${lastName}
+Email: ${email} 
+Phone number: ${phone}
+`);
 });
